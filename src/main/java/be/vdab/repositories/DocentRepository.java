8283012eefe32a1320b474ab972635071810bca1
Dimitri <dvanlooy@gmail.com>
2016-08-30 11:57:47 +0200

    BULK UPDATES EN BULK DELETES

--- src/main/java/be/vdab/repositories/DocentRepository.java
+++ src/main/java/be/vdab/repositories/DocentRepository.java
@@ -1,8 +1,14 @@
 package be.vdab.repositories;
 
-import be.vdab.entities.Docent;
+import java.math.BigDecimal;
+import java.util.List;
 
-public class DocentRepository extends AbstractRepository{
+import be.vdab.entities.Docent;
+import be.vdab.valueobjects.AantalDocentenPerWedde;
+import be.vdab.valueobjects.VoornaamEnId;
+
+public class DocentRepository extends AbstractRepository {
+	private static final String JPQL_SELECT_VAN_TOT_WEDDE = "SELECT d FROM Docent d WHERE d.wedde BETWEEN :van AND :tot ORDER BY d.wedde, d.id";
 
 	public Docent read(long id) {
 		return getEntityManager().find(Docent.class, id);
@@ -18,4 +24,31 @@ public class DocentRepository extends AbstractRepository{
 			getEntityManager().remove(docent);
 		}
 	}
+
+	public List<Docent> findByWeddeBetween(BigDecimal van, BigDecimal tot, int vanafRij, int aantalRijen) {
+		return getEntityManager().createNamedQuery("Docent.findByWeddeBetween", Docent.class).setParameter("van", van)
+				.setParameter("tot", tot).setFirstResult(vanafRij).setMaxResults(aantalRijen).getResultList();
+	}
+
+	public List<VoornaamEnId> findVoornamen() {
+		return getEntityManager()
+				.createQuery("select new be.vdab.valueobjects.VoornaamEnId(d.id, d.voornaam) from Docent d",
+						VoornaamEnId.class)
+				.getResultList();
+	}
+
+	public BigDecimal findMaxWedde() {
+		return getEntityManager().createQuery("select max(d.wedde) from Docent d", BigDecimal.class).getSingleResult();
+	}
+
+	public List<AantalDocentenPerWedde> findAantalDocentenPerWedde() {
+		return getEntityManager().createQuery("select new be.vdab.valueobjects.AantalDocentenPerWedde(d.wedde,count(d))"
+				+ " from Docent d group by d.wedde", AantalDocentenPerWedde.class).getResultList();
+	}
+	
+	public void algemeneOpslag(BigDecimal factor) {
+		getEntityManager().createNamedQuery("Docent.algemeneOpslag")
+		.setParameter("factor", factor)
+		.executeUpdate();
+		}
 }
--- src/main/java/be/vdab/services/DocentService.java
+++ src/main/java/be/vdab/services/DocentService.java
@@ -1,71 +1,97 @@
 package be.vdab.services;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import be.vdab.entities.Docent;
 import be.vdab.repositories.DocentRepository;
+import be.vdab.valueobjects.AantalDocentenPerWedde;
+import be.vdab.valueobjects.VoornaamEnId;
 
-public class DocentService extends AbstractService{
+public class DocentService extends AbstractService {
 	private final DocentRepository docentRepository = new DocentRepository();
 
 	public Docent read(long id) {
-//		EntityManager entityManager = JPAFilter.getEntityManager();
-//		try {
-//			return docentRepository.read(id, entityManager);
-//		} finally {
-//			entityManager.close();
-//		}
+		// EntityManager entityManager = JPAFilter.getEntityManager();
+		// try {
+		// return docentRepository.read(id, entityManager);
+		// } finally {
+		// entityManager.close();
+		// }
 		return docentRepository.read(id);
 	}
 
 	public void create(Docent docent) {
-//		EntityManager entityManager = JPAFilter.getEntityManager();
-//		try {
-//			entityManager.getTransaction().begin();
-//			docentRepository.create(docent, entityManager);
-//			entityManager.getTransaction().commit();
-//		} catch (RuntimeException ex) {
-//			entityManager.getTransaction().rollback();
-//			throw ex;
-//		} finally {
-//			entityManager.close();
-//		}
+		// EntityManager entityManager = JPAFilter.getEntityManager();
+		// try {
+		// entityManager.getTransaction().begin();
+		// docentRepository.create(docent, entityManager);
+		// entityManager.getTransaction().commit();
+		// } catch (RuntimeException ex) {
+		// entityManager.getTransaction().rollback();
+		// throw ex;
+		// } finally {
+		// entityManager.close();
+		// }
 		beginTransaction();
 		docentRepository.create(docent);
 		commit();
 	}
 
 	public void delete(long id) {
-//		EntityManager entityManager = JPAFilter.getEntityManager();
-//		try {
-//			entityManager.getTransaction().begin();
-//			docentRepository.delete(id, entityManager);
-//			entityManager.getTransaction().commit();
-//		} catch (RuntimeException ex) {
-//			entityManager.getTransaction().rollback();
-//			throw ex;
-//		} finally {
-//			entityManager.close();
-//		}
+		// EntityManager entityManager = JPAFilter.getEntityManager();
+		// try {
+		// entityManager.getTransaction().begin();
+		// docentRepository.delete(id, entityManager);
+		// entityManager.getTransaction().commit();
+		// } catch (RuntimeException ex) {
+		// entityManager.getTransaction().rollback();
+		// throw ex;
+		// } finally {
+		// entityManager.close();
+		// }
 		beginTransaction();
 		docentRepository.delete(id);
 		commit();
 	}
 
 	public void opslag(long id, BigDecimal percentage) {
-//		EntityManager entityManager = JPAFilter.getEntityManager();
-//		try {
-//			entityManager.getTransaction().begin();
-//			docentRepository.read(id, entityManager).opslag(percentage);
-//			entityManager.getTransaction().commit();
-//		} catch (RuntimeException ex) {
-//			entityManager.getTransaction().rollback();
-//			throw ex;
-//		} finally {
-//			entityManager.close();
-//		}
+		// EntityManager entityManager = JPAFilter.getEntityManager();
+		// try {
+		// entityManager.getTransaction().begin();
+		// docentRepository.read(id, entityManager).opslag(percentage);
+		// entityManager.getTransaction().commit();
+		// } catch (RuntimeException ex) {
+		// entityManager.getTransaction().rollback();
+		// throw ex;
+		// } finally {
+		// entityManager.close();
+		// }
 		beginTransaction();
 		docentRepository.read(id).opslag(percentage);
+		commit();
+	}
+
+	public List<Docent> findByWeddeBetween(BigDecimal van, BigDecimal tot, int vanafRij, int aantalRijen) {
+		return docentRepository.findByWeddeBetween(van, tot, vanafRij, aantalRijen);
+	}
+
+	public List<VoornaamEnId> findVoornamen() {
+		return docentRepository.findVoornamen();
+	}
+
+	public BigDecimal findMaxWedde() {
+		return docentRepository.findMaxWedde();
+	}
+
+	public List<AantalDocentenPerWedde> findAantalDocentenPerWedde() {
+		return docentRepository.findAantalDocentenPerWedde();
+	}
+
+	public void algemeneOpslag(BigDecimal percentage) {
+		BigDecimal factor = BigDecimal.ONE.add(percentage.divide(BigDecimal.valueOf(100)));
+		beginTransaction();
+		docentRepository.algemeneOpslag(factor);
 		commit();
 	}
 }
